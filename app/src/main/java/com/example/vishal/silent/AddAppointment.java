92e0008aacf14ce16d4a50aac386db50238d2c19
Kiran Saravanan <Kiran Saravanan>
2016-09-24 16:04:45 -0400

    Manual Entry test done

--- app/src/main/java/com/example/vishal/silent/AddAppointment.java
+++ app/src/main/java/com/example/vishal/silent/AddAppointment.java
@@ -1,26 +1,49 @@
 package com.example.vishal.silent;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class AddAppointment extends AppCompatActivity {
     public Appointment appointment1;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_appointment);
-        EditText appName = (EditText) findViewById(R.id.nameAppointment);
-        TimePicker timePicker1 = (TimePicker) findViewById(R.id.timePickerStart);
-        TimePicker timePicker2 = (TimePicker) findViewById(R.id.timePickerEnd);
-        appointment1 = new Appointment(appName.getText().toString(), timePicker1.getHour(),
-                timePicker1.getMinute(), timePicker2.getHour(), timePicker2.getMinute(),7);
+        btn = (Button) findViewById(R.id.addButton);
+        final EditText appName = (EditText) findViewById(R.id.nameAppointment);
+        final TimePicker timePicker1 = (TimePicker) findViewById(R.id.timePickerStart);
+        final TimePicker timePicker2 = (TimePicker) findViewById(R.id.timePickerEnd);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appointment1 = new Appointment(appName.getText().toString(), timePicker1.getHour(),
+                        timePicker1.getMinute(), timePicker2.getHour(), timePicker2.getMinute(),7);
+                changeVolume();
+
+            }
+        });
+
+
 
     }
 
     public Appointment getAppointment() {
         return appointment1;
+    }
+    public void changeVolume(){
+        SilenceEvent se = new SilenceEvent(AddAppointment.this, getAppointment());
+        se.setMuteTime();
+        se.setUnmuteTime();
     }
 
 }
