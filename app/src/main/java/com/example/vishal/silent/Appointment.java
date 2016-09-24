945e80385497a532206eca6d8900e8d278afa6ae
Vishal <vishal@Vishals-MacBook-Pro.local>
2016-09-24 04:05:58 -0400

    Added comments, changed method name to better reflect its function

--- app/src/main/java/com/example/vishal/silent/Appointment.java
+++ app/src/main/java/com/example/vishal/silent/Appointment.java
@@ -12,12 +12,16 @@ import java.util.ArrayList;
 
 public class Appointment {
              private String name; 
-            private int startTimeHour; 
-            private int startTimeMinute; 
-            private int endTimeHour; 
-            private int endTimeMinute; 
-            private int daysOfTheWeek; 
+            private int startTimeHour;  //hour value of the starting time
+            private int startTimeMinute;  //minute value of the starting time
+            private int endTimeHour;  //hour value of the ending time
+            private int endTimeMinute;  //minute value of the starting time
+            private int daysOfTheWeek;  //Day of the week with Sunday = 1 .... Saturday = 7
+            //stores collection of Appointments
             ArrayList<Appointment> eventCollection = new ArrayList<Appointment>();  
+
+            //constructor creates a new Appointment object with given attributes
+            //An Appointment is an event in the user's schedule
             public Appointment(String name, int startTimeHour, int startTimeMinute, 
                                String startTimeAMPM, String endTimeAMPM, int endTimeHour, 
                                int endTimeMinute, int daysOfTheWeek) { 
@@ -39,45 +43,59 @@ public class Appointment {
                 addToCollection(this); 
             }  
 
+            //helper method to add Appointments to the collection of Appointments
             private void addToCollection(Appointment e) { 
                 eventCollection.add(e); 
             }  
-            public void removeEvent(Appointment e) { 
+
+            //method allows removal of an existing appointment
+            public void removeAppointment(Appointment e) { 
                 eventCollection.remove(e); 
             }  
+
             public String getName() { 
                 return name; 
             }  
+
             public void setName(String name) { 
                 this.name = name; 
             }  
+
             public int getStartTimeHour() { 
                 return startTimeHour; 
             }  
             public void setStartTimeHour(int startTimeHour) { 
                 this.startTimeHour = startTimeHour; 
             }  
+
             public int getStartTimeMinute() { 
                 return startTimeMinute; 
             }  
+
             public void setStartTimeMinute(int startTimeMinute) { 
                 this.startTimeMinute = startTimeMinute; 
             }  
+
             public int getEndTimeHour() { 
                 return endTimeHour; 
             }  
+
             public void setEndTimeHour(int endTimeHour) { 
                 this.endTimeHour = endTimeHour; 
             }  
+
             public int getEndTimeMinute() { 
                 return endTimeMinute; 
             }  
+
             public void setEndTimeMinute(int endTimeMinute) { 
                 this.endTimeMinute = endTimeMinute; 
             }  
+
             public int getDaysOfTheWeek() { 
                 return daysOfTheWeek; 
             }  
+
             public void setDaysOfTheWeek(int daysOfTheWeek) { 
                 this.daysOfTheWeek = daysOfTheWeek; 
             } 
